e1aa51aa87a0232b02591a2b3cb15b15cf2e1d8c
Spencer Carli <spencercarli@gmail.com>
2019-09-26 10:17:33 -0500

    Revert commits to get app to starting point

--- mobile/App/screens/Feed.js
+++ mobile/App/screens/Feed.js
@@ -1,30 +1,17 @@
 import React from 'react';
 import { FlatList, View } from 'react-native';
-import { useQuery, useMutation } from '@apollo/react-hooks';
-
-import { requestFeed } from '../graphql/queries';
-import { likeStatus } from '../graphql/mutations';
 
 import { Status, Separator } from '../components/Status';
 
 const Feed = ({ navigation }) => {
-  const { loading, data } = useQuery(requestFeed);
-  const [likeStatusFn] = useMutation(likeStatus);
-
-  if (loading) {
-    return null;
-  }
-
   return (
     <FlatList
-      data={data.feed}
+      data={[]}
       renderItem={({ item }) => (
         <Status
           {...item}
           onRowPress={() => navigation.push('Thread', { status: item })}
-          onHeartPress={() =>
-            likeStatusFn({ variables: { statusId: item._id } })
-          }
+          onHeartPress={() => alert('todo!')}
         />
       )}
       ItemSeparatorComponent={() => <Separator />}
--- mobile/App/screens/Thread.js
+++ mobile/App/screens/Thread.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import { FlatList, View } from 'react-native';
-import { useQuery, useMutation } from '@apollo/react-hooks';
 
 import { Status, Separator } from '../components/Status';
 import { Button } from '../components/Button';
-import { requestResponses } from '../graphql/queries';
-import { likeStatus } from '../graphql/mutations';
 
 const Thread = ({ navigation }) => {
   const originalStatus = navigation.getParam('status', {});
-  const { loading, data } = useQuery(requestResponses, {
-    variables: { _id: originalStatus._id },
-  });
-  const [likeStatusFn] = useMutation(likeStatus);
-
-  if (loading) {
-    return null;
-  }
 
   return (
     <FlatList
-      data={data.responses}
+      data={[]}
       renderItem={({ item }) => (
         <Status
           {...item}
-          onHeartPress={() =>
-            likeStatusFn({ variables: { statusId: item._id } })
-          }
+          onHeartPress={() => alert('todo!')}
           indent={item._id !== originalStatus._id}
         />
       )}
