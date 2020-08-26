c1fc9c369696c3dfb4a6888cd63e747c89bb2a64
Spencer Carli <spencercarli@gmail.com>
2020-08-26 10:14:12 -0500

    Get more posts in list

--- server/db.js
+++ server/db.js
@@ -4,24 +4,54 @@ const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
 
-db.defaults({
-  feed: [
-    {
-      _id: "1",
+const posts = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "Vivamus sodales ex a nisl pellentesque laoreet.",
+  "Aliquam pellentesque turpis eget purus efficitur venenatis.",
+  "Fusce tempus tortor ac odio lacinia, sed auctor dui bibendum.",
+  "Nullam fermentum diam non nisi tincidunt, sit amet vulputate sapien dapibus.",
+  "Ut interdum magna quis nisi ornare, a imperdiet massa aliquet.",
+  "Ut at justo ut ligula luctus aliquam.",
+  "Maecenas et eros vitae purus iaculis sollicitudin at vitae tortor.",
+  "Quisque aliquet sapien porttitor, luctus leo a, sagittis libero.",
+  "In pulvinar erat iaculis, pellentesque dui ut, convallis massa.",
+  "Cras ac augue fermentum, elementum mauris sed, feugiat lorem.",
+  "Praesent interdum justo imperdiet, varius augue in, mollis mauris.",
+];
+
+const responses = [
+  "Sed facilisis nibh sed semper pulvinar.",
+  "Nulla pretium massa nec velit tincidunt facilisis.",
+  "Mauris placerat nisi at tempus porttitor.",
+];
+
+const getFeed = () => {
+  const feed = [];
+
+  posts.forEach((text, index) => {
+    feed.push({
+      _id: `${index}`,
       userId: "user-1",
-      text: "This is an example post. Isn't it great?!",
-      publishedAt: "2019-09-12T15:52:01.169Z",
+      text,
+      publishedAt: new Date().toISOString(),
       parentPostId: null,
-    },
-    {
-      _id: "t-1",
-      userId: "user-1",
-      text:
-        "This is an example response, which is really just a post with a parent. Isn't it great?!",
-      publishedAt: "2019-09-12T15:52:01.169Z",
-      parentPostId: "1",
-    },
-  ],
+    });
+  });
+
+  responses.forEach((text, index) => {
+    feed.push({
+      _id: `r-${index}`,
+      userId: "user-2",
+      text,
+      publishedAt: new Date().toISOString(),
+      parentPostId: "0",
+    });
+  });
+  return feed;
+};
+
+db.defaults({
+  feed: getFeed(),
   users: [
     {
       _id: "user-1",
