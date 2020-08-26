15ea4dcf4866caa73865ff7335443cdf82e2fe4a
Spencer Carli <spencercarli@gmail.com>
2020-08-26 11:31:18 -0500

    Use actual jwt when making requests

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const { ApolloServer } = require("apollo-server");
+const jwt = require("jsonwebtoken");
 
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
+const config = require("./config");
+const db = require("./db");
 
-const validateTokenAndGetUserId = () => {
-  // TODO: do work to authorize user
-  return "user-2";
+const validateTokenAndGetUserId = (token) => {
+  try {
+    const { _id } = jwt.verify(token, config.JWT_SECRET);
+    // Check the db to make sure they're still a user
+    const user = db.get("users").find({ _id }).value();
+    return user._id;
+  } catch {
+    // Something bad happened so we can't validate the user;
+    return null;
+  }
 };
 
 const server = new ApolloServer({
