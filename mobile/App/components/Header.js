c48fcd8d7a8c3c6fbbc29fcd992017e0d2f0af10
Spencer Carli <spencercarli@gmail.com>
2020-08-27 10:46:31 -0500

    Stub new post

---
+++ mobile/App/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { Button } from "./Button";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    alignItems: "center",
+  },
+  linkText: {
+    color: "#1ca1f2",
+    fontSize: 15,
+    marginLeft: 5,
+  },
+});
+
+export const Header = ({ onLeftPress, leftText, onRightPress, rightText }) => (
+  <SafeAreaView style={styles.container} edges={["top", "left", "right"]}>
+    <TouchableOpacity onPress={onLeftPress}>
+      <Text style={styles.linkText}>{leftText}</Text>
+    </TouchableOpacity>
+    <Button onPress={onRightPress} text={rightText} />
+  </SafeAreaView>
+);
--- mobile/App/screens/NewPost.js
+++ mobile/App/screens/NewPost.js
@@ -1,1 +1,36 @@
-export const NewPost = () => null;
+import React, { useState } from "react";
+import { ScrollView } from "react-native";
+
+import { NewPostInput } from "../components/NewPostInput";
+import { Header } from "../components/Header";
+
+export const NewPost = ({ navigation }) => {
+  const [statusText, setStatusText] = useState("");
+
+  return (
+    <>
+      <Header
+        onLeftPress={() => navigation.pop()}
+        leftText="Cancel"
+        onRightPress={() => {
+          alert(statusText);
+          navigation.pop();
+        }}
+        rightText="Post"
+      />
+
+      <ScrollView
+        style={{ backgroundColor: "rgba(27,31,35,.05)" }}
+        contentContainerStyle={{
+          flex: 1,
+          backgroundColor: "rgba(27,31,35,.05)",
+        }}
+      >
+        <NewPostInput
+          placeholder="What's the latest?"
+          onChangeText={(text) => setStatusText(text)}
+        />
+      </ScrollView>
+    </>
+  );
+};
